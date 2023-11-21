2ae71b0d2aa62c1fcb9b8a0f4850c0ced5f56d45
Muhammad-Usman36 <146926435+Muhammad-Usman36@users.noreply.github.com>
2023-11-21 11:09:33 +0500

    starting

--- App.tsx
+++ App.tsx
@@ -1,25 +1,38 @@
-import React from "react";
-import { View,Text,StyleSheet } from "react-native";
+import React,{useState} from "react";
+import { View,Text,Button,StyleSheet,TextInput,Alert } from "react-native";
 
-export const App =()=>{
-  return (
-<View style={styles.body}>  
-    <Text style={styles.text}>
-    FORMIC_PROJECT
-      </Text>
+export const App: React.FC = () => {
+  const wait_for_the_logic = () =>
+  Alert.alert([
+
+    {text: 'OK', onPress: () => console.log('OK Pressed')},
+  ]);
+  return(
+    <View style={styles.mainView}>
+      <Text style={{fontSize: 20,margin: 20}}>Please Enter</Text>
+      <TextInput style={styles.input} placeholder="Enter Name" />
+      <TextInput style={styles.input} placeholder="Enter Number" />
+      <TextInput style={styles.input} placeholder="Enter @mail.com" />
+      <Button title="ADD"onPress={() => Alert.alert('wait for the logic')}/>
+
     </View>
-);
+  );
 };
 export default App;
 
 const styles = StyleSheet.create({
-  body:{
+  mainView: {
+    //backgroundColor: 'white',
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  text:{
-    color:'white',
-    fontWeight:'bold',
+  input: {
+    height: 30,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 5,
+    marginBottom: 20,
   },
 });
