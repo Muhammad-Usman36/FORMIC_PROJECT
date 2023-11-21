7c00347b384d38ef4d802e495a1b26433ae0932d
Muhammad-Usman36 <146926435+Muhammad-Usman36@users.noreply.github.com>
2023-11-21 12:07:46 +0500

    Add structure

--- App.tsx
+++ App.tsx
@@ -2,30 +2,64 @@ import React,{useState} from "react";
 import { View,Text,Button,StyleSheet,TextInput,Alert } from "react-native";
 
 export const App: React.FC = () => {
-  const wait_for_the_logic = () =>
-  Alert.alert([
 
-    {text: 'OK', onPress: () => console.log('OK Pressed')},
-  ]);
   return(
-    <View style={styles.mainView}>
-      <Text style={{fontSize: 20,margin: 20}}>Please Enter</Text>
-      <TextInput style={styles.input} placeholder="Enter Name" />
-      <TextInput style={styles.input} placeholder="Enter Number" />
-      <TextInput style={styles.input} placeholder="Enter @mail.com" />
-      <Button title="ADD"onPress={() => Alert.alert('wait for the logic')}/>
+    <View style={{backgroundColor: 'white',flex:1}}>
+      <Text style={{color: 'black',fontWeight: 'bold', alignSelf: 'center',fontSize: 30,marginVertical: 20}}>
+        Data Entry Form
+      </Text>
+
+ <View style={styles.mainView}>
+
+<TextInput style={styles.input} placeholder="Enter first name" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>First Name</Text>
+
+
+<TextInput style={styles.input} placeholder="Enter last Name" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>last Name</Text>
+
+<TextInput style={styles.input} placeholder="Enter @Email" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>Email        </Text>
+
+
+
+<TextInput style={styles.input} placeholder="Enter Address" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>Adress      </Text>
+
+
+
+<TextInput style={styles.input} placeholder="Enter phone_number" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>P# Number</Text>
+
+
+<TextInput style={styles.input} placeholder="Enter Postal_code" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>Postal_code</Text>
+
+
+<TextInput style={styles.input} placeholder="Enter referral_code" placeholderTextColor={"black"}/>
+<Text style={{color: 'black',marginLeft: 10}}>referral_code</Text>
+<View style={{marginTop: 50}}>
+<Button title="Sign_up"onPress={() => Alert.alert('wait for the logic')}/>
+</View>
+
+</View>
+
 
     </View>
+   
+  
   );
 };
 export default App;
 
 const styles = StyleSheet.create({
   mainView: {
-    //backgroundColor: 'white',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+    flexWrap: 'wrap',
+    flexDirection:'row',
   },
   input: {
     height: 30,
