6d683a9c093d0de2f20ba7c1ee10d029060d6407
Muhammad-Usman36 <146926435+Muhammad-Usman36@users.noreply.github.com>
2023-11-23 10:18:26 +0500

    LOgic Is marge in th eUI design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View,Text,Button,StyleSheet,TextInput,Alert } from "react-native";
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
@@ -16,79 +16,109 @@ const validationSchema = yup.object().shape({
 
 });
 
-const DataEntryForm = () => {
+const App = () => {
   const handleSubmit = (values) => {
     console.log(values);
     // Handle form submission logic here
   };
 
   return (
-    <View>
+         <View style={{backgroundColor: 'white',flex:1}}>
+      <Text style={{color: 'black',fontWeight: 'bold', alignSelf: 'center',fontSize: 30,marginVertical: 20}}>
+        Data Entry Form
+      </Text>
       <Formik
         initialValues={{adress: '',Rnumber: '',Pcode: '', fname: '',lname: '', email: '', Pnumber: '' }}
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
-          <View>
+          <View style={styles.mainView}>
             <TextInput
+            style={styles.input} 
               onChangeText={handleChange('fname')}
               onBlur={handleBlur('fname')}
               value={values.fname}
               placeholder="First _ Name"
+              placeholderTextColor={"black"}
             />
             {errors.fname && <Text style={{ color: 'red' }}>{errors.fname}</Text>}
+            <Text style={{color: 'black',marginLeft: 10}}>First Name</Text>
+
 
             <TextInput
+                        style={styles.input} 
               onChangeText={handleChange('lname')}
               onBlur={handleBlur('lname')}
               value={values.lname}
               placeholder="Last _ Name"
+              placeholderTextColor={"black"}
             />
             {errors.lname && <Text style={{ color: 'red' }}>{errors.lname}</Text>}
+            <Text style={{color: 'black',marginLeft: 10}}>last Name</Text>
+
 
             <TextInput
+             style={styles.input} 
               onChangeText={handleChange('adress')}
               onBlur={handleBlur('adress')}
               value={values.adress}
               placeholder="adress"
+              placeholderTextColor={"black"}
             />
             {errors.adress && <Text style={{ color: 'red' }}>{errors.adress}</Text>}
+            <Text style={{color: 'black',marginLeft: 10}}>Adress      </Text>
 
 
             <TextInput
+              style={styles.input} 
               onChangeText={handleChange('email')}
               onBlur={handleBlur('email')}
               value={values.email}
               placeholder="Email"
+              placeholderTextColor={"black"}
             />
             {errors.email && <Text style={{ color: 'red' }}>{errors.email}</Text>}
+            <Text style={{color: 'black',marginLeft: 10}}>Email        </Text>
+
 
             <TextInput
+              style={styles.input} 
               onChangeText={handleChange('Pnumber')}
               onBlur={handleBlur('Pnumber')}
               value={values.Pnumber}
               placeholder="Phone _ number"
+              placeholderTextColor={"black"}
             />
             {errors.Pnumber && <Text style={{ color: 'red' }}>{errors.Pnumber}</Text>}
+            <Text style={{color: 'black',marginLeft: 10}}>P# Number</Text>
+
 
             <TextInput
+              style={styles.input} 
               onChangeText={handleChange('Rnumber')}
               onBlur={handleBlur('Rnumber')}
               value={values.Rnumber}
               placeholder="Refral _ number"
+              placeholderTextColor={"black"}
             />
             {errors.Rnumber && <Text style={{ color: 'red' }}>{errors.Rnumber}</Text>}
+            <Text style={{color: 'black',marginLeft: 10}}>referral_code</Text>
+
 
             <TextInput
+              style={styles.input} 
               onChangeText={handleChange('Pcode')}
               onBlur={handleBlur('Pcode')}
               value={values.Pcode}
               placeholder="Postal _ code"
+              placeholderTextColor={"black"}
             />
             {errors.Pcode && <Text style={{ color: 'red' }}>{errors.Pcode}</Text>}
-
-            <Button onPress={handleSubmit} title="Submit" />
+            <Text style={{color: 'black',marginLeft: 10}}>Postal_code</Text>
+            <View style={{marginLeft: 50,marginTop:40}}>
+<Button title="Sign_up"onPress={() => Alert.alert('wait for the logic')}/>
+</View>
           </View>
         )}
       </Formik>
@@ -96,4 +126,24 @@ const DataEntryForm = () => {
   );
 };
 
-export default DataEntryForm;
+export default App;
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    flexWrap: 'wrap',
+    flexDirection:'row',
+  },
+  input: {
+    height: 30,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 5,
+    marginBottom: 20,
+  },
+});
+
